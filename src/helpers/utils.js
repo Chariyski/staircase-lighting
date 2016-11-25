18a87c44091e362c4142ec707450bc3d3e26158b
Krasimir Chariyski <kr.chariyski@gmail.com>
2016-11-25 23:39:04 +0200

    Refactor color generating

--- src/helpers/utils.js
+++ src/helpers/utils.js
@@ -11,14 +11,7 @@ function delay(milliseconds) {
 }
 
 function randomColor() {
-  const letters = '0123456789ABCDEF';
-  let color = '#';
-
-  for (let i = 0; i < 6; i++ ) {
-      color += letters[Math.floor(Math.random() * 16)];
-  }
-
-  return color;
+  return '#' + Math.floor(Math.random()*16777215).toString(16);;
 }
 
 module.exports = {
